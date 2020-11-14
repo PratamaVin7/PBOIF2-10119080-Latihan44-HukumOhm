c603f54a833fd8fe980fa8fe80bbd6e3feaa5572
PratamaVin7 <kevinpratamaoke@gmail.com>
2020-11-14 11:58:44 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan44/hukumohm/Baterai.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan44.hukumohm;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * (OBJECT ORIENTED) Hukum Ohm
+ */
+class Baterai {
+    private float kuatArus, hambatan;
+    
+    public Baterai() {
+        System.out.println("=====Hukum Ohm=====");
+        System.out.println(
+                "Kuat arus yang mengalir pada suatu kawan penghantar\n"
+                + "akan berbanding lurus dengan beda potensial\n"
+                + "pada ujung-ujung kawat penghantar tersebut\n"
+                + "asalkan suhu kawat dijaga konstan.\n");
+    }
+    
+    public Baterai(float kuatArus, float hambatan){
+        this.kuatArus = kuatArus;
+        this.hambatan = hambatan;
+    }
+    
+    public float getKuatArus() {
+        return kuatArus;
+    }
+    
+    public float getHambatan() {
+        return hambatan;
+    }
+    
+    public float hitungTegangan() {
+        return kuatArus * hambatan;
+    }
+    
+}
---
+++ src/pboif2/pkg10119080/latihan44/hukumohm/PBOIF210119080Latihan44HukumOhm.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan44.hukumohm;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * (OBJECT ORIENTED) Hukum Ohm
+ */
+public class PBOIF210119080Latihan44HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Baterai batt = new Baterai();
+        Baterai batt1 = new Baterai(3,12);
+        System.out.println("Kuat Arus : " + batt1.getKuatArus() + " ampere");
+        System.out.println("Hambatan : " + batt1.getHambatan() + " ohm");
+        System.out.println("Hasil Tegangan : " + batt1.hitungTegangan() + " volt");
+    }
+    
+}
